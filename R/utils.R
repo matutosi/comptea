48d06e20821cf996c5e700742de7223aef29afb6
Toshikazu Matsumura <matutosi@gmail.com>
2024-08-16 03:43:54 +0900

    update

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,24 @@
+#' Helper function to create group numbers
+#'
+group_no <- function(x){
+  x |>
+    as.character() |>
+    forcats::fct_inorder() |>
+    as.numeric()
+}
+
 #' multiple version of reduce
-#' 
+#'
 preduce <- function(.l, .f, ..., .init, .dir = c("forward", "backward")){
   .dir <- match.arg(.dir)
   purrr::reduce(
-    purrr::transpose(.l), 
-    \(x, y){ rlang::exec(.f, x, !!!y, ...) }, 
+    purrr::transpose(.l),
+    \(x, y){ rlang::exec(.f, x, !!!y, ...) },
     .init = .init, .dir = .dir)
 }
 
 #' Wrapper for base::split
-#' 
+#'
 split_by <- function(df, group){
   split(df, df[[group]])
 }
